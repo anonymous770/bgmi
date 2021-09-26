efca01b7dfe46cadc9e08dc2e8e40a68e9777bbf
anonymous <64162091+anonymous770@users.noreply.github.com>
2021-09-27 03:24:44 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pkg install nano
 cp -r /sdcard/Android/data/com.pubg.imobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/UserCustom.ini ~/bgmi/
-cp -r ~/bgmi ~/../usr/bin/
+cp -r ~/bgmi/bgmi ~/../usr/bin/
 chmod +x ~/../usr/bin/bgmi
 nano ~/../usr/etc/bash.bashrc
