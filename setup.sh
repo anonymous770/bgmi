fa38ba20a9b3db10bd0c0b1423d6832724656d3e
anonymous770 <aslammiya5257@gmail.com>
2021-09-27 02:44:57 +0530

    ok

---
+++ setup.sh
@@ -0,0 +1,6 @@
+termux-setup-storage
+pkg install nano
+cp -r /sdcard/Android/data/com.pubg.imobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/UserCustom.ini .
+cp -r bgmi ~/../usr/bin/
+chmod +x ~/../usr/bin/bgmi
+nano ~/../usr/etc/bash.bashrc
